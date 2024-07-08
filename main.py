303d71acdf110dd44ac6fa2ac71d6179441491aa
Aaron10123 <chenyongrong0123@gmail.com>
2024-07-08 11:58:07 +0800

    註解應用練習

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Hello World!")
+# 可以在一行寫出多個語句
+print("Hello World!")  # 這是印出Hello World!的程式碼
+
+
+for i in range(10):  # 循環10次
+    print(i)  # 印出i的值
+
+# 注意到循環的時候，可以在循環中印出其他語句
+for i in range(10):  # 循環10次
+    print(i)  # 這是印出i的值
+# ctrl+F2 可跟改相同的地方
+# ctrl+? 可註解一行
+"""
+可註解多行
+
+"""
